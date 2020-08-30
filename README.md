ea001f9b69d432ae492a82fe737120f3ae47af45
Arjun Attam <arjunattam@gmail.com>
2020-08-30 15:42:40 -0700

    Add react-trello webview

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Codeboards
+# Codeboard
+
+## Usage
+
+### Build
+
+To build webview run the following. Webview does not support hot reload.
+
+```
+npm run build:webview
+```
+
+To develop the extension, press F5. Extension code will hot reload.
+
+### Release
+
+```
+vsce package
+```
---
+++ src/webView.ts
@@ -0,0 +1,64 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+class WebviewPanel {
+  constructor(context: vscode.ExtensionContext) {
+    const buildPath = path.join(context.extensionPath, "webview", "build");
+    const panel = vscode.window.createWebviewPanel(
+      "codeboardPanel",
+      "Codeboard",
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        localResourceRoots: [vscode.Uri.file(buildPath)],
+      }
+    );
+    panel.webview.html = getWebviewContent(buildPath);
+  }
+}
+
+function getWebviewContent(buildPath: string) {
+  const manifest = require(path.join(buildPath, "asset-manifest.json"));
+  const entrypoints = manifest["entrypoints"];
+  const stylePath = entrypoints.find((e: string) => e.endsWith(".css"));
+  const styleUri = vscode.Uri.file(path.join(buildPath, stylePath)).with({
+    scheme: "vscode-resource",
+  });
+  const scriptPaths = entrypoints.filter((e: string) => e.endsWith(".js"));
+  const scriptUris = scriptPaths.map((p: string) =>
+    vscode.Uri.file(path.join(buildPath, p)).with({ scheme: "vscode-resource" })
+  );
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <meta http-equiv="Content-Security-Policy"
+        content="default-src 'none';
+        img-src https: vscode-webview-resource: vscode-resource:;
+        script-src-elem 'unsafe-inline' https: vscode-webview-resource:;
+        style-src-elem 'unsafe-inline' https: vscode-webview-resource:;
+        script-src 'unsafe-inline' https: vscode-resource:;
+        style-src 'unsafe-inline' https: vscode-resource:;"/>
+
+    <title>React App</title>
+
+    <link rel="stylesheet" type="text/css" href="${styleUri}">
+    <base href="${vscode.Uri.file(buildPath).with({
+      scheme: "vscode-resource",
+    })}/">
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+
+    <script src="${scriptUris[0]}"></script>
+    <script src="${scriptUris[1]}"></script>
+    <script src="${scriptUris[2]}"></script>
+  </body>
+</html>`;
+}
+
+export default WebviewPanel;
